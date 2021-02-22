525818ab50812b99f75e459432f40bf15258bd6e
Tyler Epstein <tylerepstein27@gmail.com>
2021-02-22 15:23:12 -0500

    adds example paths

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,2 +1,17 @@
 class Api::ExamplePagesController < ApplicationController
+  def hello_action
+    render json: [{ message: "Hello" }, { message: "Hello again" }]
+  end
+
+  def goodbye_action
+    render json: { message: "Goodbye" }
+  end
+
+  def awesome_action
+    render json: { message: "This is kind of awesome" }
+  end
+
+  def html_action
+    render inline: "<h1>Hello</h1> <img src= 'https://www.thesprucepets.com/thmb/EBp990AJt94XwAp7oOAzUtdg9Xg=/2121x1193/smart/filters:no_upscale()/golden-retriever-puppy-in-grass-923135452-5c887d4146e0fb00013365ba.jpg'/>"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/hello_path" => "example_pages#hello_action"
+    get "/goodbye_path" => "example_pages#goodbye_action"
+    get "/awesome_path" => "example_pages#awesome_action"
+    get "/html_path" => "example_pages#html_action"
+  end
 end
